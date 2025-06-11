07bd401f7c97c4bc5da77a7cdc0fb8813878b19f
deeksha7980 <145980222+deeksha7980@users.noreply.github.com>
2025-06-12 01:43:04 +0530

    Create Employees_manager_salary.sql

---
+++ Employees_manager_salary.sql
@@ -0,0 +1,22 @@
+/* Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| salary      | int     |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+ 
+
+Write a solution to find the employees who earn more than their managers.
+
+Return the result table in any order.*/
+
+SELECT E.name AS Employee
+FROM Employee E
+JOIN Employee M ON E.managerId = M.id
+WHERE E.salary > M.salary;
